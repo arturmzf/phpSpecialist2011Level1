51cd7e0cf499675f86f4b49a9568f25b7cf7c2c9
arturmzf <artur.mzf@gmail.com>
2021-12-26 14:34:38 +0300

    Модуль 2, лабораторная работа 01

---
+++ mod2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Модуль 2</title>
+	</head>
+	<body>
+		<h1>Модуль 2</h1>
+		<a href="lab2-01.php">Лабораторная работа 01</a>
+	</body>
+</html>
---
+++ mod2/lab2-01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<mate charset="utf-8" />
+		<title>Модуль 2 | Лабораторная работа 01</title>
+	</head>
+	<body>
+		<h2>Модуль 2</h2>
+		<h1>Лабораторная работа 01</h1>
+		<?php
+			$name = "Артур";
+			$age = 29;
+			$love = "Я люблю Камиллу!";
+
+			echo "Меня зовут: ".$name."<br />";
+			echo "Мне ".$age." лет.\n"; // /n не отработает!
+			echo $love;
+
+			unset($age);
+		?>
+	</body>
+</html>
