5e8680c6efcd3563cbe1a7258b25c624b05781fd
arturmzf <artur.mzf@gmail.com>
2022-01-15 14:40:00 +0300

    Модуль 4 | Лабораторная работа 01

---
+++ mod4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Модуль 4</title>
+	</head>
+	<body>
+		<h1>Модуль 4</h1>
+		<a href="lab4-01.php">Лабораторная работа 01</a><br />
+		<a href="lab4-02.php">Лабораторная работа 02</a><br />
+		<a href="lab4-03.php">Лабораторная работа 03</a><br />
+		<a href="lab4-04.php">Лабораторная работа 04</a><br />
+        <a href="lab4-05.php">Лабораторная работа 05</a><br />
+	</body>
+</html>
